b62541dd38953ee90c70f9ff5581e0e77aea3e91
Dmitriy Germanov <seltis.gtn@gmail.com>
2024-06-23 19:58:39 +0300

    homewor completed

---
+++ FindFileWithText.cs
@@ -0,0 +1,49 @@
+﻿namespace Seminar_8
+{
+    internal class FindFileWithText
+    {
+        public Dictionary<String, List<String>> Search(string[] args)
+        {
+            if (args.Length < 3 || args.Length > 3)
+                throw new Exception("Программа принимает 3 аргумента: путь, расширение файла, искомый текст");
+            var findWords = new FindWordsInFile();
+            var fileList = SearchForFilesPaths(args[0], args[1]);
+            Dictionary<String, List<String>> result = new();
+
+            foreach (var item in fileList)
+            {
+                List<String> str = findWords.FindWords(new string[] { item, args[2] });
+                if (str.Any())
+                    result.Add(Path.GetFileName(item), str);
+            }
+            return result;
+
+        }
+        private List<string> SearchForFilesPaths(string path, string extension)
+        {
+
+            var list = new List<string>();
+            if (!Directory.Exists(path))
+                throw new Exception("Директории не существует");
+            DirectoryInfo root = new DirectoryInfo(path);
+            var files = root.GetFiles();
+            var directories = root.GetDirectories();
+
+            foreach (var item in files)
+            {
+                if (item.Name.Contains(extension))
+                {
+                    list.Add(Path.Combine(item.Directory.FullName, item.Name));
+                }
+            }
+
+
+            foreach (var item in directories)
+            {
+                list.AddRange(SearchForFilesPaths(item.FullName, extension));
+            }
+            return list;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿/*1. Напишите консольную утилиту для копирования файлов
 Пример запуска: utility.exe file1.txt file2.txt*/
+
 /*2.Напишите утилиту рекурсивного поиска файлов в заданном каталоге и подкаталогах*/
-/*Напишите утилиту читающую тестовый файл и выводящую на экран строки содержащие искомое слово.*/
+
+/*3.Напишите утилиту читающую тестовый файл и выводящую на экран строки содержащие искомое слово.*/
+
+/*HomeWork: Объедините две предыдущих работы(практические работы 2 и 3): поиск файла и поиск текста в файле написав утилиту которая ищет файлы определенного расширения с указанным текстом. Рекурсивно. Пример вызова утилиты: utility.exe txt текст.*/
 
 using Seminar_8;
 
@@ -9,6 +13,34 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        var search = new FindFileWithText();
+        var result = new Dictionary<String, List<String>>();
+        try
+        {
+            result = search.Search(args);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        if (result.Any())
+        {
+            foreach (var item in result)
+            {
+                Console.WriteLine($"Для файла {item.Key} найдены следующие строки:");
+                Console.WriteLine(String.Join("\n", item.Value));
+            }
+        }
+        else
+        {
+            Console.WriteLine("По Вашему запросу файлы не найдены!");
+        }
+
+
+
+
+        //Ниже код работы на семинаре
+
         /*    //CopyToFile.CopyTo(args);
             FindFile find = new();
             var result = new List<string>();
@@ -29,24 +61,24 @@ internal class Program
             {
                 Console.WriteLine("Файлы не найдены");
             }*/
-        var findWord = new FindWordsInFile();
-        try
-        {
-            var result = findWord.FindWords(args);
-            if(!result.Any())
-                Console.WriteLine("Слово в данном файле не найдено");
-            else
+        /*       var findWord = new FindWordsInFile();
+               try
                {
-                Console.WriteLine("Слово найдено в следующих строках:");
-                Console.WriteLine(String.Join("\n", result));
+                   var result = findWord.FindWords(args);
+                   if(!result.Any())
+                       Console.WriteLine("Слово в данном файле не найдено");
+                   else
+                      {
+                       Console.WriteLine("Слово найдено в следующих строках:");
+                       Console.WriteLine(String.Join("\n", result));
 
-            } 
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-        
+                   } 
+               }
+               catch (Exception ex)
+               {
+                   Console.WriteLine(ex.Message);
+               }*/
+
     }
 
 
